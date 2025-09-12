1c6e870ad6aeef9144d372fb2ee3b85ca2098b08
INFOR\cmartin2 <christiandaniel.martin@infor.com>
2025-09-12 08:31:03 +0800

    new

--- DBToolV2/DBTool/MainWindow.xaml.cs
+++ DBToolV2/DBTool/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using DBTool.Controls;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,16 +18,50 @@ namespace DBTool
     /// </summary>
     public partial class MainWindow : Window
     {
+        DashboardControl dashboardControl = null;
+        SettingsControl settingsControl = null;
         public MainWindow()
         {
             InitializeComponent();
+            rdoDashboard.IsChecked = true;
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            
 
-            
+        }
+
+        private void mainRdo_Checked(object sender, RoutedEventArgs e)
+        {
+            mainDockPanel.Children.Clear();
+            SetCurrentControl();
+            GetSettings();
+
+        }
+        private void SetCurrentControl()
+        {
+            if (rdoDashboard.IsChecked == true)
+            {
+                if (dashboardControl != null)
+                    dashboardControl = null;
+
+                dashboardControl = new DashboardControl();
+                mainDockPanel.Children.Add(dashboardControl);
+            }
+            if(rdoSettings.IsChecked == true)
+            {
+                if (settingsControl != null)
+                    settingsControl = null;
+
+                settingsControl = new SettingsControl();
+                mainDockPanel.Children.Add(settingsControl);
+            }
+
+        }
+
+        private void GetSettings()
+        {
+
         }
     }
 }
