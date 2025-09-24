18cced1667557390caa67a57a6eedf271f1bd2aa
INFOR\cmartin2 <christiandaniel.martin@infor.com>
2025-09-24 19:02:47 +0800

    change listview search

--- DBToolV2/DBTool/App.xaml.cs
+++ DBToolV2/DBTool/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Runtime.ConstrainedExecution;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Threading;
 
@@ -94,6 +95,62 @@ namespace DBTool
             {
                 isCell = false;
             }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox == null) return;
+
+            var listView = FindParent<ListView>(textBox);
+            if (listView == null) return;
+
+            var view = CollectionViewSource.GetDefaultView(listView.ItemsSource);
+            if (view == null) return;
+
+            string filterText = textBox.Text?.Trim() ?? string.Empty;
+
+            view.Filter = item =>
+            {
+                if (string.IsNullOrEmpty(filterText))
+                    return true;
+
+                // Look at all GridViewColumns
+                if (listView.View is GridView gridView)
+                {
+                    foreach (var col in gridView.Columns)
+                    {
+                        // Works only if using DisplayMemberBinding
+                        if (col.DisplayMemberBinding is Binding binding)
+                        {
+                            if (item is IDictionary<string, object> dict)
+                            {
+                                foreach (var value in dict.Values)
+                                {
+                                    var text = value?.ToString() ?? string.Empty;
+                                    if (text.IndexOf(filterText, StringComparison.OrdinalIgnoreCase) >= 0)
+                                        return true;
+                                }
+                            }
+                            else
+                            {
+
+                                var prop = item.GetType().GetProperty(binding.Path.Path);
+                                if (prop != null)
+                                {
+                                    var value = prop.GetValue(item)?.ToString() ?? string.Empty;
+                                    if (value.IndexOf(filterText, StringComparison.OrdinalIgnoreCase) >= 0)
+                                        return true;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return false;
+            };
+
+            view.Refresh();
         }
     }
 }
